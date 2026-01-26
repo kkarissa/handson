c1dae6a8e66b736be4abc58150d7717393162861
kkarissa <karissanicole0311@gmail.com>
2026-01-26 15:19:29 -0700

    manually merged math into main

--- B.py
+++ B.py
@@ -2,4 +2,8 @@ class B:
     pass
 
     def print_hello(this):
-        print("hello world!");
+        print("hello world!")
+
+    def print_new(this):
+        print("I know math, look:")
+        print(2 + 2)
